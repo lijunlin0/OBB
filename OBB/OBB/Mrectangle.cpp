59d9793de3fe94594cd6327b8ac0e6606400a174
lijunlin0 <117365903+lijunlin0@users.noreply.github.com>
2023-07-30 20:49:18 +0800

    优化矩形算法

--- OBB/OBB/Mrectangle.cpp
+++ OBB/OBB/Mrectangle.cpp
@@ -1,41 +1,45 @@
 #include "Mrectangle.h"
 #include <graphics.h>
 #include<iostream>
+#include<math.h>
 using namespace std;
+const double PI = 3.1415926;
 Mrectangle::Mrectangle(float x, float y, float angle, float width, float height)
 {
+	center.first = x;
+	center.second = y;
 	this->angle = angle;
 	this->width = width;
 	this->height = height;
-	int size;
-	pair<float, float> p(x, y);
-	int temp = angle;
-	for (int i = 0; i < 4; i++,temp+=90)
-	{
-		vertexs.push_back(p);
-		if (i % 2 == 0)
-		{
-			size = height;
-		}
-		else
-		{
-			size = width;
-		}
-		Line l(p.first, p.second, temp, size);
-		p = l.get_end_point();
-	}	
+	float size = sqrt(width * width + height + height) / 2;
+	float center_angle = atan(height / width) * 180 / PI;
+
+	Line l1(x, y, -center_angle + angle, size);
+	Line l2(x, y, center_angle + angle, size);
+	Line l3(x, y, 180 - center_angle + angle, size);
+	Line l4(x, y, -(180 - center_angle) + angle, size);
+
+	vertexs.push_back(l1.get_end_point());
+	vertexs.push_back(l2.get_end_point());
+	vertexs.push_back(l3.get_end_point());
+	vertexs.push_back(l4.get_end_point());
+	POINT pts[] = { {vertexs[0].first, vertexs[0].second},  {vertexs[1].first,vertexs[1].second}, {vertexs[2].first, vertexs[2].second }, { vertexs[3].first,vertexs[3].second } };
+	solidpolygon(pts, 4);
 }
 pair<float, float> Mrectangle::get_center()
 {
-	Line l(vertexs[0].first, vertexs[0].second, angle, height/2);
-	pair<float, float> center_(l.get_end_point());
-	Line temp1(center_.first, center_.second, l.get_angle() + 90, width/2);
-	return temp1.get_end_point();
+	return center;
 }
 
 pair<pair<float, float>, pair<float, float>> Mrectangle::get_axis()
 {
-	pair<float, float> axisX(vertexs[1].first - vertexs[0].first, vertexs[1].second - vertexs[0].second);
-	pair<float, float> axisY(vertexs[2].first - vertexs[1].first, vertexs[2].second - vertexs[1].second);
+	float x1 = vertexs[1].first - vertexs[0].first;
+	float y1 = vertexs[1].second - vertexs[0].second;
+	float l1 = sqrt(x1 * x1 + y1 * y1);
+	pair<float, float> axisX(x1/l1, y1/l1);
+	x1 = vertexs[2].first - vertexs[1].first;
+	y1 = vertexs[2].second - vertexs[1].second;
+	l1= sqrt(x1 * x1 + y1 * y1);
+	pair<float, float> axisY(x1 / l1, y1 / l1);
 	return pair<pair<float, float>, pair<float, float>>(axisX,axisY);
 }
--- OBB/OBB/OBB.cpp
+++ OBB/OBB/OBB.cpp
@@ -18,7 +18,7 @@ float dot(pair<float, float> p1, pair<float, float> p2)
 }
 float minmax(float a,float b,bool is_min)
 {
-	if (is_min = true)
+	if (is_min == true)
 	{
 		return a < b ? a : b;
 	}
@@ -27,25 +27,25 @@ float minmax(float a,float b,bool is_min)
 		return a > b ? a : b;
 	}
 }
+
 bool intersect(Mrectangle a, Mrectangle b, pair<float,float> axis)
 {
 	float minT1 = 99999999;
-	float maxT1 = 0;
-	float minT2 = 99999999;
-	float maxT2 = 0;
+	float maxT1 = -minT1;
+	float minT2 = minT1;
+	float maxT2 = -minT1;
 
 	for (int i = 0; i < a.get_vertexs().size(); i++)
 	{
 		float Dot = 0;
 	    //矩形a投影
 		Dot = dot(a.get_vertexs()[i], axis);
-		minT1 = min(minT1, Dot);
-		maxT1 = max(maxT1, Dot);
-
+		minT1 = minmax(minT1, Dot,true);
+		maxT1 = minmax(maxT1, Dot,false);
 		//矩形b投影
 		Dot = dot(b.get_vertexs()[i], axis);
-		minT2 = min(minT2, Dot);
-		maxT2 = max(maxT2, Dot);
+		minT2 = minmax(minT2, Dot,true);
+		maxT2 = minmax(maxT2, Dot,false);
 	}
 	if (maxT1 < minT2 || maxT2 < minT1)
 	{
@@ -61,8 +61,7 @@ void OBB(Mrectangle a,Mrectangle b)
 	bool four = intersect(a, b, b.get_axis().second);
 	if (one && two && three && four)
 	{
-		
-		setfillcolor(RED);
+		setfillcolor(LIGHTRED);
 	}
 	else
 	{
@@ -73,13 +72,8 @@ void OBB(Mrectangle a,Mrectangle b)
 void init(int x,int y,float angle1,float angle2)
 {
 	//矩形初始化
-	Mrectangle a(x, y-200, angle1, 400, 200);
-	Mrectangle b(200, 200, angle2, 400,200);
-	
-	POINT pts1[] = { {a.get_vertexs()[0].first, a.get_vertexs()[0].second},  {a.get_vertexs()[1].first, a.get_vertexs()[1].second},  {a.get_vertexs()[2].first, a.get_vertexs()[2].second}, {a.get_vertexs()[3].first, a.get_vertexs()[3].second} };
-	solidpolygon(pts1, 4);
-	POINT pts2[] = { {b.get_vertexs()[0].first, b.get_vertexs()[0].second},  {b.get_vertexs()[1].first, b.get_vertexs()[1].second},  {b.get_vertexs()[2].first, b.get_vertexs()[2].second}, {b.get_vertexs()[3].first, b.get_vertexs()[3].second} };
-	solidpolygon(pts2, 4);
+	Mrectangle a(x, y, angle1, 400, 200);
+	Mrectangle b(500, 500, angle2, 400,200);
 
 
 	OBB(a, b);
@@ -99,11 +93,13 @@ int main()
 		bool b = peekmessage(&msg, EX_MOUSE);
 		int x = msg.x;
 		int y = HEIGHT-msg.y;
+		angle1 += 0.05;
+		angle2 += 0.1;
 		BeginBatchDraw();
 
 		clearrectangle(0, HEIGHT, WIDTH, 0);
 
-		init(x,y,30,-45);
+		init(x,y, angle1,angle2);
 
 		EndBatchDraw();
 	}
